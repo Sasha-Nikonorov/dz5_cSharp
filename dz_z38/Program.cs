12d0e4686d35ead18b44b05c8452d07d025e899d
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-22 21:31:03 +0300

    dif min max array index

---
+++ dz_z38/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Введите размер массива");
+int X = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+double[] createArray()
+{
+    Random random = new Random();
+
+    double[] result = new double[X];
+
+    for (long i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(1, 100);
+    }
+    return result;
+}
+
+void Sort(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+        if (min > array[i]) min = array[i];
+    }
+    Console.WriteLine("MAX " + max);
+    Console.WriteLine("min " + min);
+    Console.WriteLine();
+    double result = max - min;
+    Console.WriteLine(result);
+}
+
+void printArray(double[] array)
+{
+    Console.Write("[");
+    
+    for (long i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+double[] array = createArray();
+
+Sort(array);
+printArray(array);
